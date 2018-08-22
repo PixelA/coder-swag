c1bf96c35c0df3e189d5f01cd44071022fef2b53
Asherlee Levy <asherleelevy@gmail.com>
2018-08-22 12:46:04 -0400

    added a data swift file and a new service group

---
+++ coder-swag/Services/DataService.swift
@@ -0,0 +1,13 @@
+//
+//  DataService.swift
+//  coder-swag
+//
+//  Created by Asherlee Levy on 8/22/18.
+//  Copyright © 2018 Asherlee Levy. All rights reserved.
+//
+
+import Foundation
+class DataService {
+    static let instance = DataService()
+    
+}
